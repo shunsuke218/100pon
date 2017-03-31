8bfa83adc3b5c004e42465acb8de309b18c46c8d
shunsuke218 <shunsuke218@gmail.com>
2017-03-31 16:21:35 -0400

    Initial commit of 6shou.

--- 5shou.py
+++ 5shou.py
@@ -14,7 +14,7 @@ file = "neko.txt.cabocha"
 
 #40. 係り受け解析結果の読み込み（形態素）
 #形態素を表すクラスMorphを実装せよ．このクラスは表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）をメンバ変数に持つこととする．さらに，CaboChaの解析結果（neko.txt.cabocha）を読み込み，各文をMorphオブジェクトのリストとして表現し，3文目の形態素列を表示せよ．
-print "\nQ40: "
+print "Q40: "
 class Morph():
     def __init__(self, surface, base, pos, pos1):
         self.surface = surface
